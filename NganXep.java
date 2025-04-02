9a90bbe68c9fff7e72921a656f87f18d72ee2ba9
nguyenhieu195 <nguyenhieu1952005@gmail.com>
2025-04-02 22:10:46 +0700

    update code

--- NganXep.java
+++ NganXep.java
@@ -29,6 +29,8 @@ class Node {
 
 class myStack {
 
+    Scanner sc = new Scanner(System.in);
+
     Node top; // đầu
 
     public myStack() { // khởi tạo ngăn xếp rỗng 
@@ -54,10 +56,10 @@ class myStack {
         return x;
     }
 
-    void nhap(Scanner sc) {
+    void nhap() {
         int x;
         while (true) {
-            System.out.println("Nhập 1 số <!0> để thêm vào Ngăn xếp: ");
+            System.out.print("Nhập 1 số (!0) để thêm vào Ngăn xếp: ");
             x = sc.nextInt();
             if (x == 0) {
                 break;
@@ -69,7 +71,7 @@ class myStack {
 
     void in() {
         myStack t = new myStack();
-        System.out.println("Nội dung ngăn xếp:");
+        System.out.print("Nội dung ngăn xếp: ");
         while (!EmptyS()) {
             int x = pop();
             System.out.print(x + " ");
@@ -79,6 +81,7 @@ class myStack {
             int x = t.pop();
             push(x);
         }
+        System.out.println();
     }
 
     void tong() {
@@ -96,22 +99,143 @@ class myStack {
         }
         System.out.println("Tổng: " + k);
     }
+
+    int demLe() {
+        int dem = 0;
+        myStack t = new myStack();
+
+        while (!EmptyS()) {
+            int x = pop();
+            t.push(x);
+        }
+        while (!t.EmptyS()) {
+            int x = t.pop();
+            if (x % 2 != 0) {
+                dem++;
+            }
+            push(x);
+        }
+        return dem;
+    }
+
+    void timMax() {
+        myStack t = new myStack();
+        int max = 0;
+        while (!EmptyS()) {
+            int x = pop();
+            System.out.println("x = " + x);
+            t.push(x);
+        }
+        while (!t.EmptyS()) {
+            int x = t.pop();
+            if (max < x) {
+                max = x;
+            }
+            push(x);
+        }
+        System.out.println("Phần tử lớn nhất trong mảng: " + max);
+    }
+
+    void xoaCuoi() {
+        if (top == null) {
+            System.out.println("Ngăn xếp rỗng!");
+            return;
+        }
+        if (top.next == null) {
+            top = null;
+            return;
+        }
+        myStack t = new myStack();
+        int dem1 = 0;
+        int dem2 = 0;
+        while (!EmptyS()) {
+            int x = pop();
+            t.push(x);
+            dem1++;
+        }
+        while (!t.EmptyS()) {
+            int x = t.pop();
+            dem2++;
+            if (dem1 > dem2) {
+                push(x);
+            } else {
+                break;
+            }
+        }
+    }
+
+    void themCuoi() {
+        myStack t = new myStack();
+        System.out.print("Nhập phần tử muốn thêm: ");
+        int a = sc.nextInt();
+
+        if (top == null) {
+            push(a);
+            return;
+        }
+
+        while (!EmptyS()) {
+            int x = pop();
+            t.push(x);
+        }
+        while (!t.EmptyS()) {
+            int x = t.pop();
+            push(x);
+        }
+        push(a);
+    }
+
+    void themX() {
+        System.out.print("Nhập phần tử muốn thêm vào ngăn xếp: ");
+        int x = sc.nextInt();
+        System.out.print("Nhập vị trí muốn thêm vào ngăn xếp: ");
+        int k = sc.nextInt();
+
+        myStack t = new myStack();
+        int dem = 0;
+        while (!EmptyS()) {
+            int a = pop();
+            t.push(a);
+        }
+        while(!t.EmptyS()){
+            int a = t.pop();
+            dem++;
+            if(dem == k){
+                push(x);
+            }
+            push(a);
+        }
+    }
 }
 
 public class NganXep {
 
     public static void main(String[] args) {
         myStack m = new myStack();
-        Scanner sc = new Scanner(System.in);
-        m.push(3);
-        m.push(2);
-        m.push(7);
-        m.push(6);
-        m.push(9);
-        while (!m.EmptyS()) {
-            System.out.println(m.pop() + " ");
-        }
-//        m.nhap(sc);
+//        m.push(3);
+//        m.push(2);
+//        m.push(7);
+//        m.push(6);
+//        m.push(9);
+//        while (!m.EmptyS()) {
+//            System.out.println(m.pop() + " ");
+//        }
+        m.nhap();
+        m.in();
+
+//        System.out.println("Số phần tử lẻ trong ngăn xếp: " + m.demLe());
+//
+//        m.timMax();
+//        System.out.println("Ngăn xếp sau khi xoá cuối: ");
+//        m.xoaCuoi();
 //        m.in();
+//
+//        System.out.println("Ngăn xếp sau khi thêm cuối: ");
+//        m.themCuoi();
+//        m.in();
+        
+        System.out.println("Ngăn xếp sau khi thêm: ");
+        m.themX();
+        m.in();
     }
 }
